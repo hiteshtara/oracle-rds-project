ff1e31592f26ed7caa20ead84b2f13b129271107
hitesh <mukadder@bu.edu>
2025-07-01 14:17:34 -0400

    ddd

--- main.tf
+++ main.tf
@@ -14,3 +14,10 @@ provider "aws" {
 
 /* --- VPC, Subnets, RDS, EC2, S3, IAM config inserted here from prior code --- */
 /* For brevity, placeholder used. User has full version already loaded. */
+output "s3_bucket_name" {
+  value = aws_s3_bucket.schema_bucket.bucket
+}
+
+output "oracle_rds_endpoint" {
+  value = aws_db_instance.oracle.endpoint
+}
